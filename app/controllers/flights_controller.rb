0bea89806aba1c011b2cb274b105c4098cf4d51c
curly08 <mattynoakes08@gmail.com>
2021-12-14 22:24:34 -0500

    Fix bug that prevented #index before a form was submitted

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,8 +1,10 @@
 class FlightsController < ApplicationController
   def index
+    return @flights = [] unless params[:search]
+
     @flights = Flight.filter(flight_search_params).map do |f|
       flight_info = "ID: #{f.id}\nLeaving: #{f.departure_airport.code}\nArriving: #{f.arrival_airport.code}\nDeparture Time: #{f.departure_time.strftime('%d/%m/%Y %I:%M%P')}"
-      [flight_info, f]
+      [flight_info, f.id]
     end
   end
 
