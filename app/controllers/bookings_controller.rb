4dedafec1abb9c57556ede8f2cd50e8c7181756b
curly08 <mattynoakes08@gmail.com>
2021-12-14 22:25:58 -0500

    Add #new action and #booking_params

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,5 +1,14 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @passenger = @booking.passengers.build
+    @flight = Flight.find(params[:book_flight][:selected_flight_id])
+    @passenger_count = params[:book_flight][:passenger_count].to_i
+  end
 
+  private
+
+  def booking_params
+    params.require(:confirm_booking).permit(:selected_flight_id, :passenger_count, passenger_attributes: [:id, :name, :email])
   end
 end
