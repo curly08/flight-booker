df59aa7b546a2dec171a9b58e4a4101dda1b604d
curly08 <mattynoakes08@gmail.com>
2021-12-15 19:10:00 -0500

    --amend

--- db/migrate/20211214004455_create_bookings.rb
+++ db/migrate/20211214004455_create_bookings.rb
@@ -2,7 +2,6 @@ class CreateBookings < ActiveRecord::Migration[6.1]
   def change
     create_table :bookings do |t|
       t.references :flight, foreign_key: { to_table: :flights }
-      t.references :passenger, foreign_key: { to_table: :passengers }
 
       t.timestamps
     end
--- db/migrate/20211215023725_remove_booking_key.rb
+++
@@ -1,6 +0,0 @@
-class RemoveBookingKey < ActiveRecord::Migration[6.1]
-  def change
-    remove_foreign_key :bookings, :passengers
-    remove_column :bookings, :passenger_id, index: true
-  end
-end
